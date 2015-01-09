93b50bfc5f88b6247b5569abb1f9afbd7aa8e583
labananala <hkim524@gmail.com>
2015-01-09 13:32:46 -0500

    Added more variables (commented out because I'm working on initialization) Modified setRange()

--- ratSlam/src/main_ratslam.java
+++ ratSlam/src/main_ratslam.java
@@ -13,7 +13,6 @@
 //    'POSECELL_VTRANS_SCALING', 2, ...
 //    'VTRANS_SCALE', 1, ... % to get the data into delta change in radians between frames
 //    'EXP_LOOPS', 200);
-import javax.media.*;
 import javax.swing.GrayFilter;
 
 import java.awt.Frame;
@@ -23,10 +22,8 @@ import java.awt.image.BufferedImage;
 import java.awt.image.FilteredImageSource;
 import java.awt.image.ImageFilter;
 import java.awt.image.ImageProducer;
-import java.io.File;
 
-public class main_ratslam 
-{
+public class main_ratslam {
 	// My Constants
 	static final double PI = Math.PI;
 
@@ -50,12 +47,23 @@ public class main_ratslam
 	double PC_C_SIZE_TH = (2 * PI) / PC_DIM_TH;
 
 	// Lookups to wrap the pose cell excitation/inhibition weight steps
-	double PC_E_XY_WRAP;
-	// continue later
+	double PC_E_XY_WRAP;	// = [(PC_DIM_XY - PC_W_E_DIM_HALF + 1):PC_DIM_XY 1:PC_DIM_XY 1:PC_W_E_DIM_HALF];
+	double PC_E_TH_WRAP;	// = [(PC_DIM_TH - PC_W_E_DIM_HALF + 1):PC_DIM_TH 1:PC_DIM_TH 1:PC_W_E_DIM_HALF];
+	double PC_I_XY_WRAP;	// = [(PC_DIM_XY - PC_W_I_DIM_HALF + 1):PC_DIM_XY 1:PC_DIM_XY 1:PC_W_I_DIM_HALF];
+	double PC_I_TH_WRAP;	// = [(PC_DIM_TH - PC_W_I_DIM_HALF + 1):PC_DIM_TH 1:PC_DIM_TH 1:PC_W_I_DIM_HALF];
+	// CONTINUE LATER
 
 	// Lookups for finding the center of the posecell Posecells by rs_get_posecell_xyth()
-	double PC_XY_SUM_SIN_LOOKUP;
-	// continue later 
+	// HK EDIT START
+	double PC_XY_SUM_SIN_LOOKUP;	// = sin((1:PC_DIM_XY).*2*pi/PC_DIM_XY);
+	double PC_XY_SUM_COS_LOOKUP;	// = cos((1:PC_DIM_XY).*2*pi/PC_DIM_XY);
+	double PC_TH_SUM_SIN_LOOKUP;	// = sin((1:PC_DIM_TH).*2*pi/PC_DIM_TH);
+	double PC_TH_SUM_COS_LOOKUP;	// = cos((1:PC_DIM_TH).*2*pi/PC_DIM_TH);
+	double PC_CELLS_TO_AVG = 3;
+	double PC_AVG_XY_WRAP ;		// = [(PC_DIM_XY - PC_CELLS_TO_AVG + 1):PC_DIM_XY 1:PC_DIM_XY 1:PC_CELLS_TO_AVG];
+	double PC_AVG_TH_WRAP;		// = [(PC_DIM_TH - PC_CELLS_TO_AVG + 1):PC_DIM_TH 1:PC_DIM_TH 1:PC_CELLS_TO_AVG];
+	// HK EDIT END
+	// CONTINUE LATER
 
 	// Specify the movie and the frames to read
 	static int START_FRAME = 1;
@@ -67,15 +75,14 @@ public class main_ratslam
 	static int RENDER_RATE = 1;
 	static int BLOCK_READ = 10;
 
-
 	static int[][][] Posecells =  new int[PC_DIM_XY][PC_DIM_XY][PC_DIM_TH];
 
 	// Other variables
-	static int[] IMAGE_VT_Y_RANGE = setRange(IMAGE_Y_SIZE);
-	static int[] IMAGE_VT_X_RANGE = setRange(IMAGE_X_SIZE);
-	static int[] IMAGE_ODO_X_RANGE = setRange(IMAGE_X_SIZE);
-	static int[] IMAGE_VTRANS_Y_RANGE = setRange(IMAGE_Y_SIZE);
-	static int[] IMAGE_VROT_Y_RANGE = setRange(IMAGE_Y_SIZE);
+	static int[] IMAGE_VT_Y_RANGE = setRange(1, IMAGE_Y_SIZE);
+	static int[] IMAGE_VT_X_RANGE = setRange(1, IMAGE_X_SIZE);
+	static int[] IMAGE_ODO_X_RANGE = setRange(1, IMAGE_X_SIZE);
+	static int[] IMAGE_VTRANS_Y_RANGE = setRange(1, IMAGE_Y_SIZE);
+	static int[] IMAGE_VROT_Y_RANGE = setRange(1, IMAGE_Y_SIZE);
 	static int[][] prev_vrot_image_x_sums;
 	static int[][] prev_vtrans_image_x_sums;
 
@@ -84,8 +91,7 @@ public class main_ratslam
 	
 	static String MOV_FILE = "file:///Users/bentito/Downloads/stlucia_testloop.avi";
 	// Main method of ratSLAM, not including constants (which are above, for the most part)
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		// Set initial position in the pose network
 		int x_pc = (int) (Math.floor(PC_DIM_XY / 2.0) + 1);
 		int y_pc = (int) (Math.floor(PC_DIM_XY / 2.0) + 1);
@@ -110,7 +116,7 @@ public class main_ratslam
 		int numvts = 1;
 		
 		// Need to fix parameters; more specifically, array sizes
-		vt[1] = new VT(numvts, new int[][]{},1.0,x_pc,y_pc,th_pc,1,1,new Experience[5]);
+		vt[1] = new VT(numvts, new int[][]{},1.0,x_pc,y_pc,th_pc,1,1, new Experience[5]);
 		vt[numvts].template_decay = 1.0;
 		Experience[] exps = new Experience[5];
 		// figure out where to get id
@@ -121,17 +127,54 @@ public class main_ratslam
 		// varargin: input variable in a function definition statement that allows the function to accept any number of input arguments 
 		//	1xN cell array, in which N is the number of inputs that the function receives after explicitly declared inputs;
 		// 	use prompt.in
-		for (int i = 0; i < (args.length - 3); i++)
-		{
+		for (int i = 0; i < (args.length - 3); i++) {
 			// figure out varargin in Java
 			// should be an array that is passed to the main(?) - double check
-			switch(args[i])
-			{
-			case "RENDER_RATE": RENDER_RATE = Integer.parseInt(args[i+1]);
-			break;
-			case "BLOCK_READ": BLOCK_READ = Integer.parseInt(args[i+1]);
-			break;
-			// ...
+			switch(args[i]) {
+				case "RENDER_RATE": RENDER_RATE = Integer.parseInt(args[i+1]);
+					break;
+				case "BLOCK_READ": BLOCK_READ = Integer.parseInt(args[i+1]);
+					break;
+				// HK EDIT START
+				case "START_FRAME": START_FRAME = Integer.parseInt(args[i+1]);
+					break;
+				case "END_FRAME": END_FRAME = Integer.parseInt(args[i+1]);
+					break;
+					
+				case "PC_VT_INJECT_ENERGY": PC_VT_INJECT_ENERGY = Integer.parseInt(args[i+1]);
+					break;
+				case "IMAGE_VT_Y_RANGE": IMAGE_VT_Y_RANGE = setRange(Integer.parseInt(args[i+1]), Integer.parseInt(args[i+2]));
+					break;
+				case "IMAGE_VT_X_RANGE": IMAGE_VT_X_RANGE = setRange(Integer.parseInt(args[i+1]), Integer.parseInt(args[i+2]));
+//					break;
+//				case "VT_SHIFT_MATCH": VT_SHIFT_MATCH = Integer.parseInt(args[i+1]);
+//					break;
+//				case "VT_MATCH_THRESHOLD": VT_MATCH_THRESHOLD = Integer.parseInt(args[i+1]);
+//					break;
+//					
+//				case "VTRANS_SCALE": VTRANS_SCALE = Integer.parseInt(args[i+1]);
+//					break;
+//				case "VISUAL_ODO_SHIFT_MATCH": VISUAL_ODO_SHIFT_MATCH = Integer.parseInt(args[i+1]);
+//					break;
+//				case "IMAGE_VTRANS_Y_RANGE": IMAGE_VTRANS_Y_RANGE = Integer.parseInt(args[i+1]);
+//					break;
+//				case "IMAGE_VROT_Y_RANGE": IMAGE_VROT_Y_RANGE = Integer.parseInt(args[i+1]);
+//					break;
+//				case "IMAGE_ODO_X_RANGE": IMAGE_ODO_X_RANGE = Integer.parseInt(args[i+1]);
+//					break;
+//				
+//				case "EXP_DELTA_PC_THRESHOLD": EXP_DELTA_PC_THRESHOLD = Integer.parseInt(args[i+1]);
+//					break;
+//				case "EXP_CORRECTION": EXP_CORRECTION = Integer.parseInt(args[i+1]);
+//					break;
+//				case "EXP_LOOPS": EXP_LOOPS = Integer.parseInt(args[i+1]);
+//					break;
+//				
+//				case "ODO_ROT_SCALING": ODO_ROT_SCALING = Integer.parseInt(args[i+1]);
+//					break;
+//				case "POSECELL_VTRANS_SCALING": POSECELL_VTRANS_SCALING = Integer.parseInt(args[i+1]);
+//					break;
+				// HK EDIT END
 			}
 
 			vt[1].template = new int[1][IMAGE_VT_X_RANGE.length];
@@ -179,12 +222,11 @@ public class main_ratslam
 	}
 
 	// Creates 1-dimensional arrays that mimic MATLAB colon operator
-	private static int[] setRange(int range)
-	{
-		int[] toReturn = new int[range];
-		for (int i = 0; i < range; i++)
-		{
-			toReturn[i] = i;
+	// HK EDIT (to allow for range that doesn't start with 1)
+	private static int[] setRange(int lower, int upper) {
+		int[] toReturn = new int[upper - lower + 1];
+		for (int i = 0, j = lower; i < toReturn.length && j <= upper; i++, j++) {
+			toReturn[i] = j;
 		}
 		return toReturn;
 	}
@@ -196,7 +238,6 @@ public class main_ratslam
 	        try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 	        System.out.println("Image at index: "+index); 
